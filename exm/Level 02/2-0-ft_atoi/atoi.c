10450e42a681eda15ba3f14128e1deb8d7de0116
Ahaif <haifouli.abdelmoughite@gmail.com>
2025-02-19 11:16:21 +0200

    shell01

---
+++ exm/Level 02/2-0-ft_atoi/atoi.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <stdlib>
+
+
+
+
+int	ft_atoi(const char *str)
+{
+    int i = 0
+
+    while(str[i] == ' ' | str[i] == '/t')
+        i++
+
+    while(str[i] >= '0' && str[i] <= 0)
+    {
+        
+    }
+
+}
+
+
+
+int main()
+{
+    char *str = "   1234sufdjgf";
+    char *tex = "dfji1516   ";
+
+    int res = atoi(str);
+    int res1 = atoi(tex);
+
+}
